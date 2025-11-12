67d2b3e908521267e97b0bf44ed63fc88c6c78aa
Gokuldas Shenoy.K <74420066+Gokuldas-dev@users.noreply.github.com>
2025-11-13 05:03:38 +0530

    Added file via git
    this is the new added code

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-resouce "aws_instance" "MY_terrafom_EC2" { 
+resource "aws_instance" "MY_terrafom_EC2" { 
   ami             ="ami-0cae6d6fe6048ca2c"
   instance_type   ="t2.micro"
 
   tags            ={
-    Name ="Hello my first instance using terraform "
+    #Name ="Hello my first instance using terraform "   # this was the firstname i given 
+    Name ="Instance_1"                                  # this was the second name i given to create using tf
   }
 }
